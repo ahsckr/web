4fdb2d8f138dca891550796a66426eae2af85789
ahsckr <147144837+ahsckr@users.noreply.github.com>
2024-04-16 10:33:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
-let topla = (a, b) =>
-{ document.write(a + b);}
-function carpma(a, b){
-//return a * b;
-document.getElementById("etiket1").inner+HTML = a * b;
+let topla = (a,b) =>
+{ document.write(a+b);}
+
+function carpma(a,b){
+//return a*b;
+document.getElementById("etiket1").innerHTML = a*b;
 }
 
-function hesapla()
-{
+function hesapla(){
 let s1 = document.getElementById("sayi1").value;
 let s2 = document.getElementById("sayi2").value;
-document.getElementById("p1").innerHTML = s1 * s2;
-//document.getElementById("sayi1").type = "checkbox";
-document.getElementsByTagName("p")[0].innerHTML = s1 * s2;
-document.getElementsByTagName("p1")[1].innerHTML = s1 * s2;
+//document.getElementById("p1").innerHTML = s1*s2;
+
+document.getElementsByTagName("p")[0].innerHTML = s1*s2;
+document.getElementsByTagName("p")[1].innerHTML = s1*s2;
+//document.getElementsByClassName("p")[1].innerHTML = s1*s2;
+//document.getElementsByName("p")[1].innerHTML = s1*s2;
+
+document.getElementById("sayi1").type="checkbox";
 
 }
